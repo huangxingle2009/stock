8de463b172ad1e8c9b2077d05345bd1dfd1f0c32
huangbule <3314308761@qq.com>
2018-06-05 19:51:10 +0800

    1

--- health.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-count=`ps -ef | grep artisan | grep qq:send | wc -l`
-#echo $count
-if [ 0 == $count ]; then
-    nohup /usr/bin/php /data/html/stock/artisan qq:send > /data/logs/qq-send.log 2>&1 &
-fi
